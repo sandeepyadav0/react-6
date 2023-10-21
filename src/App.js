b7220fb9aae22303a805c44961db1ed0be68ab98
sandeepyadav0 <sandeepyadav27577@gmail.com>
2023-10-21 21:07:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+import Navbar from './components/Navbar/Navbar.js';
+import About from './components/About/About.js';
+import Skills from './components/Skills/Skills.js';
+import Projects from './components/Projects/Projects.js';
+import ContactMe from './Components/ContactMe/ContactMe';
+import Education from './Components/Education/Education';
+
+
+const App = () => {
+    return ( <
+        div className = "App" >
+        <
+        Navbar / >
+        <
+        About / >
+        <
+        Skills / >
+        <
+        Projects / >
+        <
+        Education / >
+        <
+        ContactMe / >
+
+        <
+        /div>
+    );
+};
 
 export default App;
---
+++ src/Components/Education/Education.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Education.css';
+const Education = () => {
+    return ( <
+        div >
+        <
+        section id = "education" >
+        <
+        h2 className = "education-heading" > Education < /h2>
+
+        <
+        div className = "education-box" >
+
+        <
+        div className = "education-content" >
+        <
+        p className = 'school-name' > Vellore Institue of Technology, Amaravati < /p> <
+        p className = 'branch' > B.Tech in Computer Science and Engineering < /p> <
+        p className = 'grade' > Grade: 8.85 CGPA < /p> <
+        p className = 'year' > Year of passout: 2025 < /p> <
+        /div> <
+        /div> <
+        div className = "education-box2" >
+        <
+        div className = "education-content" >
+        <
+        p className = 'school-name' > SR Junior College < /p> <
+        p className = 'branch' > Intermediate - MPC < /p> <
+        p className = 'grade' > Grade: 75 % < /p> <
+        p className = 'year' > Year of passout: 2021 < /p> <
+        /div> <
+        /div> <
+        div className = "education-box3" >
+        <
+        div className = "education-content" >
+        <
+        p className = 'school-name' > Rishi High School < /p> <
+        p className = 'branch' > Class X, SSC < /p> <
+        p className = 'grade' > Grade: 9.2 GPA < /p> <
+        p className = 'year' > Year of passout: 2019 < /p> <
+        /div> <
+        /div> <
+        /section> <
+        /div>
+    );
+};
+
+export default Education;
